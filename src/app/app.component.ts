bda17bcba34218d57f74c121b2460ccb232ce643
Huyen Luu <huyen.luu@gendigital.com>
2023-10-26 11:11:12 +0300

    initial add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'homes';
 }
